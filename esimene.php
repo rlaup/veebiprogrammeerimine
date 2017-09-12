4230fd1220a438a3653ec23ba58ef5cc50bb2d1c
“Ranno <rlaup@users.noreply.github.com>
2017-09-12 14:17:46 +0300

    Lisatud on esimene jupp PHP koodi

---
+++ esimene.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset= "utf-8">
+	<title>Ranno Laup veebiprogrammeerimine</title>
+</head>
+<body>
+	<h1>Ranno Laup veebiprogrammeerimine</h1>
+	<p>See leht on loodud õppetöö raames ning ei sisalda mingit tõsiselt võeatavat sisu.</p>
+	<p>Noo kui täitsa aus olla, siis see sisu on ikka väga tõsine ja sünge.</p>
+	
+	<?php
+		echo "See on esimene jupp PHP abil väljastatud teksti!";
+	?>
+
+</body>
+</html>
